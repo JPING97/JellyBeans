512a99718cb8e5c2624ccd736da4d1e710c9e22d
JPING97 <jonathan_pingle@Jonathans-MacBook-Pro.local>
2015-02-01 10:39:44 -0500

    Started Setting up framework
    … again

---
+++ src/main/java/com/jping97/jellybeans/JellyBeans.java
@@ -0,0 +1,37 @@
+package com.jping97.jellybeans;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+
+/**
+ * Created by JPING97 on 2/1/15.
+ */
+
+
+@Mod(modid = "JellyBeans", name = "Jelly Beans", version = "1.7.10-1.0")
+public class JellyBeans
+{
+    @Mod.Instance("JellyBeans")
+    public static JellyBeans instance;
+
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+}
